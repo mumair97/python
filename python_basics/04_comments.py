daab60afe961eca531725dcc05040fc516724412
mumair97 <mumair@uni-bremen.de>
2023-01-08 16:27:49 +0100

    strings, input variables, conditional logics

---
+++ python_basics/04_comments.py
@@ -0,0 +1,6 @@
+# press (Ctrl + /) for single or multiple lines comments together
+
+print("how are you")      
+print("we are learning python with Ammar") # print a string
+
+print(2+6) # print operators function with numbers
